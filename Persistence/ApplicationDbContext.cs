cfbc1007d90b5808874c4a5cb39ecc3f2719253f
andre <andrew205103@gmail.com>
2023-11-19 20:47:34 +0300

    Added DbContext and Entityes classes

---
+++ Persistence/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Application.Interfaces;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public DbSet<Material> Materials { get; set; }
+
+        public DbSet<Seller> Sellers { get; set; }
+
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new MaterialConfiguration());
+            base.OnModelCreating(builder);
+        }
+    }
+}
